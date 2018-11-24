ed2489ee1580cb18209a1bb9d5e47fa804ee5942
Nobu <calro1123@yahoo.co.jp>
2018-11-24 11:28:40 +0900

    quiita

--- main3.py
+++ main3.py
@@ -93,7 +93,7 @@ def handle_message(event):
         line_bot_api.reply_message(
         event.reply_token,
         # ===================================================
-        TextSendMessage(text=str(f))
+        TextSendMessage(text=f)
 # ----------------------------------------------------------------
             
         )
